a7f9b9db8c48eb989f45bb18902486ce2bbd4e9e
AlvarezJhonn-FS <JSAlvarezMolina@student.fullsail.edu>
2025-07-07 22:19:20 -0400

    Add unique homepage message

---
+++ static-site/src/pages/index.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <h1>Hello, this is my deployed static site for the assignment!</h1>
+  );
+};
+
+export default IndexPage;
